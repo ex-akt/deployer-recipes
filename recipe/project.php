5e6921b49c2acec1f51f2ab40fe3911fce04bc56
Samuel Heer <samuel@ex-akt.de>
2023-08-15 11:04:32 +0200

    import order changed to overwrite

--- recipe/project.php
+++ recipe/project.php
@@ -2,8 +2,7 @@
 
 namespace Deployer;
 
-import('recipe/contao.php');
+import(__DIR__.'/../../../nutshell-framework/deployer-recipes/recipe/project.php');
+
 import(__DIR__ . '/contao.php');
 import(__DIR__ . '/encore.php');
-
-import(__DIR__.'/../../../nutshell-framework/deployer-recipes/recipe/project.php');
