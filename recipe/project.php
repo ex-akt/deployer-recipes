c3f92ef2c6640b27cf5c069aa7103a0903561db4
Samuel Heer <samuel@ex-akt.de>
2025-02-10 14:41:04 +0100

    add to project

--- recipe/project.php
+++ recipe/project.php
@@ -6,3 +6,5 @@ import(__DIR__.'/../../../nutshell-framework/deployer-recipes/recipe/project.php
 
 import(__DIR__ . '/contao.php');
 import(__DIR__ . '/encore.php');
+import(__DIR__ . '/isotope.php');
+import(__DIR__ . '/layout.php');
