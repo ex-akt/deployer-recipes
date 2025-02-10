b83d60cc281c25ecd786b1912a6595b47e76735b
Samuel Heer <samuel@ex-akt.de>
2025-02-10 14:49:57 +0100

    encore optin

--- recipe/encore.php
+++ recipe/encore.php
@@ -6,5 +6,6 @@ task('deploy:encore:compile', function () {
     runLocally('npm --prefix vendor/ex-akt/contao-webpack-config-bundle run prod');
 })->desc('Compile assets');
 
-before('deploy', 'deploy:encore:compile');
+
+//before('deploy', 'deploy:encore:compile');
 
