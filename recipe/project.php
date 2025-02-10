1d7c3b9dff4107f3ddbc85c3dd390b997d516bbc
Samuel Heer <samuel@ex-akt.de>
2025-02-10 14:43:28 +0100

    add files upload

--- recipe/project.php
+++ recipe/project.php
@@ -4,6 +4,7 @@ namespace Deployer;
 
 import(__DIR__.'/../../../nutshell-framework/deployer-recipes/recipe/project.php');
 
+import(__DIR__ . '/files.php');
 import(__DIR__ . '/contao.php');
 import(__DIR__ . '/encore.php');
 import(__DIR__ . '/isotope.php');
